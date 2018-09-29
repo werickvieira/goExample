049639d9c84a22d762c8858780bcb9cffca6def6
Wérick Vieira <werickvs@gmail.com>
2018-09-29 17:27:10 -0300

    Testando a requisicao, adicionando funcao para tratar erro e criando modelo para o retorno do json

--- main.go
+++ main.go
@@ -3,14 +3,37 @@ package main
 import (
 	"log"
 	"net/http"
+	"encoding/json"
 )
+
+// Message Model
+type Message struct {
+	Total int64
+}
+
+// ErrorMessage Model
+type ErrorMessage struct {
+	Message string
+	CodeError int
+}
 
 func setHeaders(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
 }
 
+func errorHandler(w http.ResponseWriter, status int) {
+	w.WriteHeader(status)
+	response := ErrorMessage{"Dominio nao encontrado", status}
+	json.NewEncoder(w).Encode(response)
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	setHeaders(w)
+	response, err := http.Get("https://www.americanas.com.br/")
+	if err != nil {
+		errorHandler(w, http.StatusNotFound)
+		return
+	}
 }
 
 func main() {
